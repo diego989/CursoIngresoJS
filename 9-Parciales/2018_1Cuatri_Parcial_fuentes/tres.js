dd7118dde279961424bb787736c38bd6fe2516f1
diego989 <52511665+diego989@users.noreply.github.com>
2019-07-06 11:45:11 -0300

    Ejercicios del 1 al 3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,17 @@
 function mostrar()
 {
+    var precio;
+    var descuento;
+    var preciofinal;
+
+    precio = prompt("Ingrese el precio: ");
+    precio = parseInt(precio);
+
+    descuento = prompt("Ingrese el descuento a aplicar: ");
+    descuento = parseInt(descuento);
+
+    preciofinal = precio-precio*descuento/100;
+
+    document.getElementById("elPrecioFinal").value = preciofinal;
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,18 @@
 
 function mostrar()
 {
+    var ancho;
+    var largo;
+    var perimetro;
+
+    ancho = prompt("Ingrese el ancho del rectangulo: ");
+    ancho = parseInt(ancho);
+
+    largo = prompt("Ingrese el largo del rectangulo: ");
+    largo = parseInt(largo);
+
+    perimetro = ancho+largo*2;
+
+    alert("El Perímetro del recrángulo es: "+perimetro);
 
 }
