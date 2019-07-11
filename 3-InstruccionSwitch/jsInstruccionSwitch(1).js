ca79a4839f0c145d2f491a24cc6ae31397813a9b
diego989 <52511665+diego989@users.noreply.github.com>
2019-07-10 22:22:08 -0300

    ejercicios del 1 al 4

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,6 +3,61 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+    switch (mesDelAño)
+         {
+            case "Enero":
+            {
+                alert("Que comiences bien el año");
+                break;
+            }
+                    case "Febrero":
+                        {
+                            break;
+                        }
+                        case "Marzo":
+                            {
+                                alert("A clases!!!!");
+                                break;
+                            }
+                            case "Abril":
+                                {
+                                    break;
+                                }
+                                case "Mayo":
+                                {
+                                    break;
+                                }
+                                     case "Junio":
+                                        {
+                                         break;
+                                        }
+                                        case "Julio":
+                                             {
+                                            alert("Se vienen las vacaciones!!!");
+                                             break;
+                                             }
+                                             case "Agosto":
+                                                {
+                                                break;
+                                                }
+                                                case "Septiembre":
+                                                    {
+                                                        break;
+                                                    }
+                                                    case "Octubre":
+                                                        {
+                                                            break;
+                                                        }
+                                                        case "Noviembre":
+                                                            {
+                                                                break;
+                                                            }
+                                                            case "Diciembre":
+                                                                {
+                                                                    alert("Felices Fiestas");
+                                                                    break;
+                                                                }
+          }
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,7 +1,74 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAño;
+mesDelAño = document.getElementById('mes').value;
+
+switch (mesDelAño)
+{
+   case "Enero":
+   {
+       alert("Este mes tiene 30 días o más.");
+       break;
+   }
+           case "Febrero":
+               {
+                   alert("Este mes no tiene más de 29 días");
+                   break;
+               }
+               case "Marzo":
+                   {
+                       alert("Este mes tiene 30 días o más.");
+                       break;
+                   }
+                   case "Abril":
+                       {
+                           alert("Este mes tiene 30 días o más.");
+                           break;
+                       }
+                       case "Mayo":
+                       {
+                           alert("Este mes tiene 30 días o más.");
+                           break;
+                       }
+                            case "Junio":
+                               {
+                                alert("Este mes tiene 30 días o más.");
+                                break;
+                               }
+                               case "Julio":
+                                    {
+                                    alert("Este mes tiene 30 días o más.");
+                                    break;
+                                    }
+                                    case "Agosto":
+                                       {
+                                        alert("Este mes tiene 30 días o más.");
+                                        break;
+                                       }
+                                       case "Septiembre":
+                                           {
+                                               alert("Este mes tiene 30 días o más.");
+                                               break;
+                                           }
+                                           case "Octubre":
+                                               {
+                                                  alert("Este mes tiene 30 días o más.");
+                                                   break;
+                                               }
+                                               case "Noviembre":
+                                                   {
+                                                       alert("Este mes tiene 30 días o más.");
+                                                       break;
+                                                   }
+                                                   case "Diciembre":
+                                                       {
+                                                          alert("Este mes tiene 30 días o más.");
+                                                           break;
+                                                       }
+ }
+
+
 
 //alert (mesDelAño);
 	
