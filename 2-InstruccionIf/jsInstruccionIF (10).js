d548a063a0c0b78df6a77605d139cea0b2ec95c5
diego989 <52511665+diego989@users.noreply.github.com>
2019-07-10 22:20:23 -0300

    ejercicios 9 y 10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,21 @@
 function mostrar()
-{
-	//Genero el número RANDOM entre 1 y 10 
-	
+{//Genero el número RANDOM entre 1 y 10 
+	var num;
+
+	num = Math.floor(Math.random()*(11-1))+1; 
+
+	if(num>=9 && num<=10)
+		{
+			alert("EXCELENTE");
+		}
+			else if(num>=4)
+				{
+					alert("APROBO");
+					}	
+
+				else 
+					{
+						alert("Vamos, la próxima se puede");
+						}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,10 @@
 function mostrar()
-{
-	//Genero el número RANDOM entre 1 y 10 
+{//Genero el número RANDOM entre 1 y 10
+	var num;
+
+	num = Math.floor(Math.random()*(11-1))+1; 
+
+	alert(num);
 	
 
 }//FIN DE LA FUNCIÓN
