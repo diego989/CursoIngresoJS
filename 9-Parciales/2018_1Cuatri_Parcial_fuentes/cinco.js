831e89703c85346c00f41c8b7d658172e01e2b89
Diego <52511665+diego989@users.noreply.github.com>
2019-07-12 22:03:26 -0300

    ejercicios 4 y 5

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,39 @@
 function mostrar()
 {
 
+    var planetas;
+    var leyenda;
+
+    planetas = prompt("Ingrese un planeta");
+
+    switch (planetas)
+    {
+        case "mercurio":
+        case "venus": 
+            {
+                leyenda="Acá hace mas calor";
+                break;
+            }   
+        case "tierra":   
+            {
+                leyenda="Acá vivimos";
+                break;
+            }    
+        case "marte":       
+        case "jupiter":  
+        case "saturno":
+        case "urano":
+        case "neptuno":  
+            {
+                leyenda="Acá hace mas frío";
+                break;
+            }
+        
+        default:
+            {
+                leyenda="Planeta inválido";
+                break;
+            }    
+    }
+alert(leyenda);            
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,36 @@
 function mostrar()
 {
+    var PrimerNum;
+    var SegundoNum;
+    var resultado;
 
+    PrimerNum = prompt("Ingrese el primer número");
+
+    SegundoNum = prompt("Ingrese el segundo número");
+   
+    if (PrimerNum == SegundoNum)
+    {
+        resultado=(PrimerNum+SegundoNum);
+    }
+    else 
+    {  
+        PrimerNum = parseInt(PrimerNum);
+        SegundoNum = parseInt(SegundoNum);
+
+        if (PrimerNum>SegundoNum)
+        {
+               
+                resultado = PrimerNum-SegundoNum;
+                
+        }
+    else 
+         {
+            resultado = PrimerNum+SegundoNum;
+            if (resultado>10)
+             { 
+              resultado = "La suma es: "+resultado+ " y supera el 10";
+             }
+         }
+    }
+    alert(resultado);
 }
