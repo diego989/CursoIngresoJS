8dfc8281bec0a1ccf4879b76c6ee6869f6f71452
Diego <52511665+diego989@users.noreply.github.com>
2019-07-12 22:04:40 -0300

    ejercicio 5

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -2,6 +2,93 @@ function mostrar()
 {
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
+laHora = parseInt(laHora);
+switch(laHora){
+
+    case 1:{
+        break;
+    }
+    case 2:{
+        break;
+    }
+    case 3:{
+        break;
+    }
+    case 4:{
+        break;
+    }
+    case 5:{
+        break;
+    }
+    case 6:{
+        break;
+    }
+    case 7:{
+        alert("Es de mañana");
+        break;
+    }
+    case 8:{
+        alert("Es de mañana");
+        break;
+    }
+    case 9:{
+        alert("Es de mañana");
+        break;
+    }
+    case 10:{
+        alert("Es de mañana");
+        break;
+    }
+    case 11:{
+        alert("Es de mañana");
+        break;
+    }
+    case 12:{
+        break;
+    }
+    case 13:{
+        break;
+    }
+    case 14:{
+        break;
+    }
+    case 15:{
+        break;
+    }
+    case 16:{
+        break;
+    }
+    case 17:{
+        break;
+    }
+    case 18:{
+        break;
+    }
+    case 19:{
+        break;
+    }
+    case 20:{
+        break;
+    }
+    case 21:{
+        break;
+    }
+    case 22:{
+        break;
+    }
+    case 23:{
+        break;
+    }
+    case 24:{
+        break;
+    }
+    default:{
+        alert("Hora Incorrecta");
+        break;
+    }    
+
+
+}
 
 //alert (laHora);
 	
