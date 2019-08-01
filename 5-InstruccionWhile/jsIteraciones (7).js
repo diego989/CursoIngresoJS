8440f24577865c4cc658016d810fbd43f3e13904
Diego <52511665+diego989@users.noreply.github.com>
2019-07-31 22:00:37 -0300

    modificacion

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,18 +3,23 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
+	var respuesta;
 	var numero;
 
 
-	while(respuesta=="si")
+	do
 	{
 		numero = prompt("Ingrese numero");
+		while (isNaN(numero))
+		{
+			numero = prompt("Ingrese un número válido nuevamente");
+		}
 		numero = parseInt(numero);
 		contador++;
 		acumulador=acumulador+numero;
 		respuesta = prompt("Ingrese si para cargar dato");
 	}
+	while (respuesta=="si");
 
 
 document.getElementById('suma').value=acumulador;
